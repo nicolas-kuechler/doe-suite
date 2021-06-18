1a7cc7187ff343cd96442e48cc79515472f9d96b
Nicolas Küchler <nico.kuechler@protonmail.com>
2021-06-18 13:45:18 +0200

    update demo code

--- demo/client.py
+++ demo/client.py
@@ -1,10 +1,14 @@
-import sys, time
+import os, sys, time
+
+print(f"cwd = {os.getcwd()}")
+
+import numpy as np
 
 print("Client Starting...")
 time.sleep(30) # wait 30 seconds
 
 print("Client Writing Output...")
-with open("results/client_out.txt") as f:
+with open("results/client_out.txt", 'w+') as f:
     f.write("Client Output - args:")
     f.write(sys.argv[1:])
 
--- demo/server.py
+++ demo/server.py
@@ -1,10 +1,14 @@
-import sys, time
+import os, sys, time
+
+print(f"cwd = {os.getcwd()}")
+
+import numpy as np
 
 print("Server Starting...")
 time.sleep(30) # wait 30 seconds
 
 print("Server Writing Output...")
-with open("results/server_out.txt") as f:
+with open("results/server_out.txt", "w+") as f:
     f.write("Server Output - args:")
     f.write(sys.argv[1:])
 
