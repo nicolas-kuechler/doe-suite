baefb7e797dcf667b9f24e4ac40b73d7cffd79d0
Nicolas Küchler <nico.kuechler@protonmail.com>
2022-08-22 11:07:35 +0200

    setup eni

--- src/roles/suite-cloud-instances-create/filter_plugins/helpers.py
+++ src/roles/suite-cloud-instances-create/filter_plugins/helpers.py
@@ -1,3 +1,69 @@
+from urllib.parse import non_hierarchical
+
+
+def to_network(my_host, eni_assignment):
+    """
+    if my_host has an eni assignment -> use it.
+    """
+    if my_host['host_type'] in eni_assignment:
+        eni_id = eni_assignment[my_host['host_type']][my_host['idx']]
+        return {'interfaces': [eni_id]}
+    else:
+        return {'assign_public_ip': True}
+
+
+def to_eni_assignment(ec2_eni_info, ec2_instance_info, host_types, host_type_specific_vars):
+    """
+    if host types have the variable `attach_eni: True` in their host specific vars,
+    then we find an existing (not assigned) eni to attach.
+    (enough eni need to exist in group_vars/all -> exp_base: eni: [])
+    """
+
+    #print(f"eni_info={ec2_eni_info}")
+    #print(f"\n\nec2_instance_info={ec2_instance_info}")
+    #print(f"host_types={host_types}")
+    #print(f"host_type_specific_vars={host_type_specific_vars}")
+
+    d_tags = {}
+    for x in ec2_instance_info["instances"]:
+        d_tags[x['instance_id']] = x['tags']
+
+    assignment = {}
+
+    # init the assignment
+    for host_type, vars in host_type_specific_vars.items():
+        if vars.get("attach_eni", False):
+            count = 0
+            for exp in host_types[host_type].keys():
+                count += host_types[host_type][exp]["n"]
+            assignment[host_type] = count * [None]
+
+    available_eni_ids = []
+    for eni in ec2_eni_info["network_interfaces"]:
+        if "attachment" in eni:
+            # assign existing eni to same host_type
+            instance_id = eni["attachment"]["instance_id"]
+            if instance_id in d_tags:
+                host_type = d_tags[instance_id]["host_type"]
+                idx = int(d_tags[instance_id]["idx"])
+                if host_type in assignment:
+                    assignment[host_type][idx] = eni["network_interface_id"]
+        else: # -> eni is available
+            available_eni_ids.append(eni["network_interface_id"])
+
+
+    for host_type in assignment.keys():
+        for i in range(len(assignment[host_type])):
+            if assignment[host_type][i] is None:
+                eni_id = available_eni_ids.pop()
+                if eni_id is None:
+                    raise ValueError(f"Not enough eni available -> create more via group_vars/all")
+                assignment[host_type][i] = eni_id
+
+
+    return assignment
+
+
 
 
 def to_tag_assignment(ec2_instances_info, host_types):
@@ -73,4 +139,6 @@ class FilterModule(object):
     def filters(self):
         return {
             'to_tag_assignment': to_tag_assignment,
+            'to_eni_assignment': to_eni_assignment,
+            'to_network': to_network,
         }
