f4c42d7a3fbcc13c6db94491ec9a86297729d1c9
Hidde L <mail.hidde@gmail.com>
2024-06-19 16:16:11 +0200

    artifact

--- doespy/doespy/util.py
+++ doespy/doespy/util.py
@@ -94,7 +94,8 @@ def get_suite_roles_dir():
 
 def get_results_dir():
     prj_dir = get_project_dir()
-    results_dir = os.path.join(prj_dir, "doe-suite-results")
+    results_dir_name = os.environ.get("DOES_RESULTS_DIR", "doe-suite-results")
+    results_dir = os.path.join(prj_dir, results_dir_name)
     return results_dir
 
 
